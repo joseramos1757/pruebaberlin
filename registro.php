baa1c542c2610f5891dfc9f771126881792454e4
joseramos1757 <jose.ramos@uab.edu.bo>
2023-03-17 17:34:48 -0400

    subiendo plantilla

--- registro.php
+++ registro.php
@@ -21,50 +21,33 @@
             <!-- Background image for card set in CSS! -->
           </div>
           <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-5 fw-light fs-5">Register</h5>
+            <h5 class="card-title text-center mb-5 fw-light fs-5">REGISTRO</h5>
             <form>
 
               <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingInputUsername" placeholder="myusername" required autofocus>
-                <label for="floatingInputUsername">Username</label>
+                <input type="text" class="form-control" id="nombre" placeholder="nombre" required autofocus>
+                <label for="nombre">NOMBRE</label>
               </div>
 
               <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInputEmail" placeholder="name@example.com">
-                <label for="floatingInputEmail">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                <label for="email">CORREO ELECTRONICO</label>
               </div>
 
               <hr>
 
               <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
-              </div>
-
-              <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPasswordConfirm" placeholder="Confirm Password">
-                <label for="floatingPasswordConfirm">Confirm Password</label>
+                <input type="password" class="form-control" id="password" placeholder="password">
+                <label for="password">CONTRASEÑA</label>
               </div>
 
               <div class="d-grid mb-2">
-                <button class="btn btn-lg btn-primary btn-login fw-bold text-uppercase" type="submit">Register</button>
+                <button class="btn btn-lg btn-primary btn-login fw-bold text-uppercase" type="submit">REGISTRAR</button>
               </div>
 
-              <a class="d-block text-center mt-2 small" href="#">Have an account? Sign In</a>
+              <a class="d-block text-center mt-2 small" href="#">si ya tienes una cuenta, ingresa</a>
 
               <hr class="my-4">
-
-              <div class="d-grid mb-2">
-                <button class="btn btn-lg btn-google btn-login fw-bold text-uppercase" type="submit">
-                  <i class="fab fa-google me-2"></i> Sign up with Google
-                </button>
-              </div>
-
-              <div class="d-grid">
-                <button class="btn btn-lg btn-facebook btn-login fw-bold text-uppercase" type="submit">
-                  <i class="fab fa-facebook-f me-2"></i> Sign up with Facebook
-                </button>
-              </div>
 
             </form>
           </div>
