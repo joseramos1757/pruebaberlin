227faf7d0d5aa0abe082cf10a723b47b1c624f98
joseramos1757 <jose.ramos@uab.edu.bo>
2023-03-17 16:39:57 -0400

    enviar

--- registro.php
+++ registro.php
@@ -9,6 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/registro.css">
 </head>
 <body>
 <body>
